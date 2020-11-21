49996b40c5586d640c0d3572dba07414b83ef9c4
Kelvin Pan <kelvin.z.pan@gmail.com>
2020-11-21 15:27:26 -0800

    Jumping and beginning of separate player model

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,17 +8,24 @@ public class Player : Mirror.NetworkBehaviour
     public float MoveSpeed;
     public float GroundSpeedLimit;
     public float TurnSmoothTime;
+    public float JumpSpeed;
+
+    public GameObject childModel;
 
     private Camera playerCam;
     private CinemachineFreeLook cmFreeLook;
     private Rigidbody rb;
+    private Collider col;
+    private RaycastHit rcHitGround;
     private float turnSmoothVelocity;
+    private float distToGround;
 
     // Called once at initialization
     void Awake()
     {
         cmFreeLook = GameObject.FindGameObjectWithTag("CameraController").GetComponent<CinemachineFreeLook>();
         rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
     }
 
     // Start is called before the first frame update
@@ -26,11 +33,12 @@ public class Player : Mirror.NetworkBehaviour
     {
         if (isLocalPlayer)
         {
-            playerCam = Camera.main;
-
             // Set up third person camera
+            playerCam = Camera.main;
             cmFreeLook.Follow = this.transform;
             cmFreeLook.LookAt = this.transform;
+
+            distToGround = col.bounds.extents.y;
         }
     }
 
@@ -40,23 +48,43 @@ public class Player : Mirror.NetworkBehaviour
         handleMovement();
     }
 
-    private void handleMovement()
+    void handleMovement()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        // Explained in Brackeys: https://www.youtube.com/watch?v=4HpC--2iowE
-        // which moves on a 2D plane and uses kinematics instead of rigidbody.
-        // Can be changed, this just simulates Super Monkey Ball
-        if (direction.magnitude >= 0.1f && rb.velocity.magnitude < GroundSpeedLimit)
+        if (isGrounded())
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCam.transform.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, TurnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            GetComponent<Rigidbody>().AddForce(moveDir.normalized * MoveSpeed * Time.deltaTime);
+            // Explained in Brackeys: https://www.youtube.com/watch?v=4HpC--2iowE
+            // which moves on a 2D plane and uses kinematics instead of rigidbody.
+            // Can be changed, this just simulates Super Monkey Ball
+            if (direction.magnitude >= 0.1f && rb.velocity.magnitude < GroundSpeedLimit)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCam.transform.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, TurnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                rb.AddForce(moveDir.normalized * MoveSpeed * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Vector3 moveDir = rcHitGround.normal;
+                rb.AddForce(moveDir.normalized * JumpSpeed);
+            }
         }
+
+    }
+
+    bool isGrounded()
+    {
+        if (Physics.Raycast(transform.position, -Vector3.up, out rcHitGround, 50f))
+        {
+            return rcHitGround.distance < (distToGround + 0.1f);
+        }
+
+        return false;
     }
 }
