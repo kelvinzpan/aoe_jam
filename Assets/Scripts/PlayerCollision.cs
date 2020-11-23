cfd46ad37633e5b5e19c33a514ddad89ecb7c6d1
Poom Sethabutr <Seth Sethabutr>
2020-11-22 16:06:21 -0800

    Added snow trail particles

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -6,11 +6,32 @@ public class PlayerCollision : MonoBehaviour
     public Transform Spawnpoint;
     public GameObject Prefab;
 
+    void Start()
+    {
+        var Snow = GetComponent<ParticleSystem>().emission;
+        Snow.enabled = false;
+    }
+
     void OnCollisionEnter(Collision collisionInfo){
         Debug.Log(collisionInfo.collider.tag);
         if(collisionInfo.collider.tag == "Obstacle"){
             KeepScore.Score += 100;
             Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation);
+        }
+
+        else if (collisionInfo.collider.tag == "Terrain")
+        {
+            var Snow = GetComponent<ParticleSystem>().emission;
+            Snow.enabled = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Terrain")
+        {
+            var Snow = GetComponent<ParticleSystem>().emission;
+            Snow.enabled = false;
         }
     }
 }
